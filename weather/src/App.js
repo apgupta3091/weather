41105b44bb265a6436b4cee3362a41c0b2ead3db
Anuj Gupta <apgupta3091@gmail.com>
2022-03-23 17:40:59 -0400

    weather not working

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,10 +1,67 @@
 
 import './App.scss';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
+
+  const [data, setData] = useState({})
+  let hi = "hi"
+  
+  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=40.71&lon=-74.00&exclude=hourly,minutely,currently&appid=8e69cb5b2085f8b636a1c05b630e8493`
+
+  
+  useEffect(() => {
+    axios.get(url).then(res => {
+      setData(data.res)
+      console.log(data.res)
+      console.log('hi')
+    })
+  },[hi])
+
+
   return (
     <div className="App">
-      hello
+      <div className="weather-container">
+        <div className="weather-item">
+          <h2>Wed</h2>
+          <div className="min-max-container">
+            <p>30</p>
+            <p>40</p>
+          </div>
+        </div>
+        <div className="weather-item">
+          <h2>Wed</h2>
+          <div className="min-max-container">
+            <p>30</p>
+            <p>40</p>
+          </div>
+        </div>
+        <div className="weather-item">
+          <h2>Wed</h2>
+          <div className="min-max-container">
+            <p>30</p>
+            <p>40</p>
+          </div>
+        </div>
+        <div className="weather-item">
+          <h2>Wed</h2>
+          <div className="min-max-container">
+            <p>30</p>
+            <p>40</p>
+          </div>
+        </div>
+        <div className="weather-item">
+          <h2>Wed</h2>
+          <div className="min-max-container">
+            <p>30</p>
+            <p>40</p>
+          </div>
+        </div>
+        
+         
+       
+      </div>
     </div>
   );
 }
